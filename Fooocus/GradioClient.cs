e4c53f622b6f516e262b85173ebacff14b0958df
雨雨 宋 <s084088@163.com>
2023-08-30 18:01:10 +0800

    添加项目文件。

---
+++ Fooocus/GradioClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Fooocus;
+public class GradioClient
+{
+    JObject json;
+
+    public async Task<string> SendAsync(params object[] parameters)
+    {
+        string hash = Guid.NewGuid().ToString("N")[..12];
+
+        ClientWebSocket webSocket = new ClientWebSocket();
+        await webSocket.ConnectAsync(new Uri("ws://127.0.0.1:7860/queue/join"), CancellationToken.None);
+
+        //等待回信
+        string result = string.Empty;
+
+        while (result != "send_hash") result = await GetJsonValue(webSocket, "msg");
+
+        await SendJson(webSocket, new { fn_index = 4, session_hash = hash });
+        while (result != "send_data") result = await GetJsonValue(webSocket, "msg");
+
+        var payload = new
+        {
+            data = parameters,
+            fn_index = 4,
+            session_hash = hash
+        };
+
+        await SendJson(webSocket, payload);
+
+        while (result != "process_completed") result = await GetJsonValue(webSocket, "msg");
+
+
+        string s = json["output"]["data"][3]["value"][0]["name"].ToString();
+
+        return s;
+
+    }
+
+    private async Task SendJson(ClientWebSocket webSocket, object o)
+    {
+        string jsonPayload = JsonConvert.SerializeObject(o);
+        byte[] sendBuffer = Encoding.UTF8.GetBytes(jsonPayload);
+        await webSocket.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    private async Task<string> GetJsonValue(ClientWebSocket webSocket, string key)
+    {
+        byte[] data = await GetBytes(webSocket);
+
+        string result = Encoding.UTF8.GetString(data);
+
+        json = JObject.Parse(result);
+
+        Debug.WriteLine(json[key].ToString());
+
+        return json[key].ToString();
+    }
+
+    private async Task<byte[]> GetBytes(ClientWebSocket webSocket)
+    {
+        byte[] receiveBuffer = new byte[4096];  // 增加缓冲区大小
+
+        using MemoryStream ms = new MemoryStream();
+        WebSocketReceiveResult result;
+        Debug.WriteLine("1:" + webSocket.State);
+        do
+        {
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+            ms.Write(receiveBuffer, 0, result.Count);  // 写入MemoryStream
+        }
+        while (!result.EndOfMessage);
+        Debug.WriteLine("2:" + webSocket.State);
+
+        byte[] data = ms.ToArray();
+
+        return data;
+    }
+}
---
+++ Fooocus/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fooocus;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+
+
+    private async Task TTT()
+    {
+        GradioClient client = new GradioClient();
+        int seed = new Random().Next(0, 1024 * 1024 * 1024);
+        try
+        {
+            string path = await client.SendAsync(
+                "2 boy chatting on the road",      //提示词
+                "",                                     //反向提示词
+                "sai-comic book",                                 //样式
+                "Quality",                              //速度还是质量  Speed  Quality
+                "1344×768",                             //分辨率
+                1,                                      //图片数量
+                seed,                                      //种子
+                2,                                      //清晰度 0-40
+                "sd_xl_base_1.0_0.9vae.safetensors",    //文生图模型
+                "sd_xl_refiner_1.0_0.9vae.safetensors", //图生图模型
+                "sd_xl_offset_example-lora_1.0.safetensors",
+                0.5,
+                "None",
+                0.5,
+                "None",
+                0.5,
+                "None",
+                0.5,
+                "None",
+                0.5
+            );
+
+
+            I1.Source = new BitmapImage(new Uri(path));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        await TTT();
+    }
+}
